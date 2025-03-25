76f5356b47b56b6c0b052f118846f0058d28865a
pkushal05 <pkushal1052@gmail.com>
2025-03-25 15:26:29 -0400

    A4P2 - Commit 3 Declared array and object for images

--- Lab4/Part2/main.js
+++ Lab4/Part2/main.js
@@ -12,10 +12,21 @@ const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
 
+const images = ["pic1.jpg", "pic2.jpg", "pic3.jpg", "pic4.jpg", "pic5.jpg"];
 
 /* Declaring the alternative text for each image file */
 
+const obj = {
+    "pic1.jpg": "Picture of Human eye",
+    "pic2.jpg": "Picture of something wavy",
+    "pic3.jpg": "Lavender flowers",
+    "pic4.jpg": "Ancient Egypt painting",
+    "pic5.jpg": "Butterfly image"
+}
+
 /* Looping through images */
+
+
 
 const newImage = document.createElement('img');
 newImage.setAttribute('src', xxx);
