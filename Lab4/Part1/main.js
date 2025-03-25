5f7f66aa6475dca05a47e7c65c3c3ab3a5c36d46
pkushal05 <pkushal1052@gmail.com>
2025-03-25 14:46:51 -0400

    A4P1 - Commit 2 Added comment header in both files

--- Lab4/Part1/main.js
+++ Lab4/Part1/main.js
@@ -1,4 +1,10 @@
-1. COMPLETE VARIABLE AND FUNCTION DEFINITIONS
+/*
+    Name: Kushal Patel
+    File: Assignment 4, Part 1
+    Date: 25th March, 2025
+    Desc: Story Generator
+*/
+
 
 const customName = document.getElementById('customname');
 const randomize = document.querySelector('.randomize');
@@ -9,7 +15,7 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-2. RAW TEXT STRINGS
+
 
 It was 94 fahrenheit outside, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.
 
@@ -25,7 +31,6 @@ spontaneously combusted
 melted into a puddle on the sidewalk
 turned into a slug and crawled away
 
-3. EVENT LISTENER AND PARTIAL FUNCTION DEFINITION
 
 randomize.addEventListener('click', result);
 
