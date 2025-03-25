2e0c82f61aec890dd73f33d20626c4692c11b991
pkushal05 <pkushal1052@gmail.com>
2025-03-25 14:55:32 -0400

    A4P1 - Commit 4 Worked on the result function

--- Lab4/Part1/main.js
+++ Lab4/Part1/main.js
@@ -27,9 +27,19 @@ const insertZ = ["spontaneously combusted", "melted into a puddle on the sidewal
 randomize.addEventListener('click', result);
 
 function result() {
+    let newStory = storyText;
+
+    const xItem = randomValueFromArray(insertX);
+    const yItem = randomValueFromArray(insertY);
+    const zItem = randomValueFromArray(insertZ);
+
+    newStory = newStory.replaceAll(":insertx:", xItem);
+    newStory = newStory.replaceAll(":inserty:", yItem);
+    newStory = newStory.replaceAll(":insertz:", zItem);
 
   if(customName.value !== '') {
     const name = customName.value;
+    newStory = newStory.replaceAll("Bob", name);
 
   }
 
